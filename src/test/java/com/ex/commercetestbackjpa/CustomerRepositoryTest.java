7fb760885b20d12ba50c682dcb12adb9b8f28c95
joonxpear <j-bae@kakao.com>
2022-10-23 17:02:06 +0900

    (테스트) 고객저장

--- src/test/java/com/ex/commercetestbackjpa/CustomerRepositoryTest.java
+++ src/test/java/com/ex/commercetestbackjpa/CustomerRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.ex.commercetestbackjpa;
 
 import com.ex.commercetestbackjpa.domain.entity.customer.Customer;
+import com.ex.commercetestbackjpa.domain.entity.customer.dto.CustomerRequestDto;
+import com.ex.commercetestbackjpa.domain.entity.customer.dto.CustomerResponseDto;
 import com.ex.commercetestbackjpa.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,14 @@ public class CustomerRepositoryTest {
 
     @Test
     public void testClass(){
+        CustomerRequestDto customerRequestDto = new CustomerRequestDto();
 
+//        customerRequestDto.setEmail("shnam@cware.co.kr");
+//        customerRequestDto.setName("남시훈");
+        customerRequestDto.setEmail("jbae@cware.co.kr");
+        customerRequestDto.setName("배준");
+        customerRequestDto.setPassword("1q2w34r!");
+
+        customerRepository.save(customerRequestDto.toEntity());
     }
 }
